e84461fa830cee22734412f6991dbb14cc1fe4d7
krndaun <74673518+krndaun@users.noreply.github.com>
2024-12-04 17:37:04 +0900

    Generate splash screen using flutter_native_splash

---
+++ lib/core/constants/app_colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primaryColor = Color(0xFF00BFA5);
+  static const secondaryColor = Color(0xFF00796B);
+}
---
+++ lib/routes/route_generator.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:pmnesysworks_v01_1204_ni/features/home/view/home_view.dart';
+
+class RouteGenerator {
+  static Route<dynamic> generateRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case '/':
+        return MaterialPageRoute(builder: (_) => HomeView());
+      default:
+        return MaterialPageRoute(
+          builder: (_) => Scaffold(
+            body: Center(child: Text('No route defined for ${settings.name}')),
+          ),
+        );
+    }
+  }
+}
